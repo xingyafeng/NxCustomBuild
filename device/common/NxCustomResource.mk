804824899579977b3d18cfb016db47dc9943b001
baiw <baiw@yunovo.cn>
2018-11-02 14:59:25 +0800

    [NxCustomBuild] 添加编译支持选项YOcCoreServer | add compile config YOcCoreServer
    Change-Id: Idef1b3129a81efbe67182adf8478bf5a2ce2b2fc

--- device/common/NxCustomResource.mk
+++ device/common/NxCustomResource.mk
@@ -48,6 +48,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += libyov lights.$(TARGET_BOARD_PLATFORM)
 
+## 提供一些给APP层面使用的接口和一些调试功能
+PRODUCT_PACKAGES += YOcCoreServer
+
 ## Zen 支持客制化项目的overlay
 PRODUCT_PACKAGE_OVERLAYS += $(YUNOVO_ROOT)/$(YUNOVO_CONFIG)/$(YUNOVO_CUSTOM_P)/overlay
 
