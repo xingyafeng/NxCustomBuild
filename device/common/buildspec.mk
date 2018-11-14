e58397a7402b591d736a3e8b82834ea1f9a55363
xingyafeng <xingyf@yunovo.cn>
2018-11-14 17:11:19 +0800

    增加[buildspec.mk]
    Change-Id: Iee72915bbb5f7a4c5647af5582ce2a6e0cc99e62

---
+++ device/common/buildspec.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+######################################################################
+# This is a do-nothing template file.  To use it, copy it to a file
+# named "buildspec.mk" in the root directory, and uncomment or change
+# the variables necessary for your desired configuration.  The file
+# "buildspec.mk" should never be checked in to source control.
+######################################################################
+
+## PRODUCT_DEFAULT_DEV_CERTIFICATE 默认系统签名
+PRODUCT_DEFAULT_DEV_CERTIFICATE := build/target/product/security/testkey
