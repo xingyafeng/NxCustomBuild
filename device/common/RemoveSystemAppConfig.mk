97f0e23b51d0b97b9e6347dab92eecf1236acfe1
时汉文 <shihanwen@yunovo.cn>
2019-01-03 13:08:38 +0000

    Revert "[Heaven]Update:cm01删除一些不需要的系统应用。"
    This reverts commit 25035d2923bc50eb610e988416cab2dabfe7c9f1.
    
    Change-Id: I82a277584869e285fc020093a4401ec5171ee0c1

--- device/common/RemoveSystemAppConfig.mk
+++ device/common/RemoveSystemAppConfig.mk
@@ -4,66 +4,18 @@
 ifneq ($(strip $(SPT_VERSION_NO)),)
   ifeq ($(strip $(call get_yov_board)), cm01)
     Y_REMOVE_PRODUCT_PACKAGES += \
-          Calendar \
-          Music \
-          MusicFX \
-          batterywarning \
-          SchedulePowerOnOff \
-          BatteryWarning \
-          SDKGallery \
-          Gallery2 \
-          MtkBt \
-          mtkbt \
-          SoundRecorder
-#add by Heaven start
-    Y_REMOVE_PRODUCT_PACKAGES += \
-          WallpaperCropper \
-          DownloadProvider \
-          CalendarProvider \
-          BasicDreams \
-          DownloadProviderUi \
-          CalendarImporter \
-          PrintSpooler \
-          BSPTelephonyDevTool \
-          AtciService \
-          LiveWallpapersPicker \
-          LiveWallpapers \
-          VisualizationWallpapers \
-          Galaxy4 \
-          HoloSpiralWallpaper \
-          NoiseField \
-          PhaseBeam \
-          MTKThermalManager \
-          SmartcardService \
-          DrmProvider \
-          Omacp \
-          Dialer \
-          OneTimeInitializer \
-          KeyChain \
-          ManagedProvisioning \
-          EngineerMode \
-          Contacts \
-          ContactsProvider \
-          PhotoTable \
-          FMRadio \
-          HTMLViewer \
-          LatinIME \
-          Stk1 \
-          VpnDialogs \
-          DeskClock \
-          Email \
-          QuickSearchBox \
-          MediaProvider \
-          FwkPlugin \
-          FWUpgrade \
-          PacProcessor \
-          NlpService \
-          ProxyHandler \
-          FWUpgradeProvider \
-          UserDictionaryProvider \
-          MTKAndroidSuiteDaemon \
-          Bluetooth
-#add by Heaven end
+      Calendar \
+	  Music \
+	  MusicFX \
+	  batterywarning \
+	  SchedulePowerOnOff \
+	  BatteryWarning \
+	  SDKGallery \
+	  Gallery2 \
+	  MtkBt \
+	  mtkbt \
+	  SoundRecorder \
+	  Calendar
   endif
 
   ifeq ($(strip $(call get_yov_board)), ms16)
@@ -71,38 +23,38 @@ ifneq ($(strip $(SPT_VERSION_NO)),)
       Calendar \
       Launcher3\
       Home\
-          Music \
-          MusicFX \
-          Gallery2 \
-          SoundRecorder \
-          Calendar \
-          Email \
-          kedaxunfei\
-          BtReverse\
-          driveRecorder\
-          EmailDrmAddon\
-          NoiseField\
-          LiveWallpapers\
-          LiveWallpapersPicker\
-          PhaseBeam\
-          PicoTts\
-          PrintSpooler
+	  Music \
+	  MusicFX \
+	  Gallery2 \
+	  SoundRecorder \
+	  Calendar \
+	  Email \
+	  kedaxunfei\
+	  BtReverse\
+	  driveRecorder\
+	  EmailDrmAddon\
+	  NoiseField\
+	  LiveWallpapers\
+	  LiveWallpapersPicker\
+	  PhaseBeam\
+	  PicoTts\
+	  PrintSpooler
   endif
 
   ifeq ($(strip $(call get_yov_board)), k68c)
     Y_REMOVE_PRODUCT_PACKAGES += \
-          Home \
-          Launcher2 \
-          Launcher3 \
-          DeskClock \
-          Email \
-          Exchange2 \
-          QuickSearchBox \
-          HoloSpiralWallpaper \
-          LiveWallpapers \
-          LiveWallpapersPicker \
-          MagicSmokeWallpapers \
-          Music \
-          MusicFX
+	  Home \
+	  Launcher2 \
+	  Launcher3 \
+	  DeskClock \
+	  Email \
+	  Exchange2 \
+	  QuickSearchBox \
+	  HoloSpiralWallpaper \
+	  LiveWallpapers \
+	  LiveWallpapersPicker \
+	  MagicSmokeWallpapers \
+	  Music \
+	  MusicFX
   endif
 endif
