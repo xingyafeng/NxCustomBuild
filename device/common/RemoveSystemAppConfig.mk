d8fbde4fcca52c1bc1b7e31c265dd715a536731f
shihanwen <shihanwen@yunovo.cn>
2019-01-04 09:20:30 +0800

    [Heaven]Update:CM01删除一些不需要的应用
    Change-Id: Ia4492c6fb3d76d65f05efa929f56201b5e2947e7

--- device/common/RemoveSystemAppConfig.mk
+++ device/common/RemoveSystemAppConfig.mk
@@ -4,57 +4,103 @@
 ifneq ($(strip $(SPT_VERSION_NO)),)
   ifeq ($(strip $(call get_yov_board)), cm01)
     Y_REMOVE_PRODUCT_PACKAGES += \
-      Calendar \
-	  Music \
-	  MusicFX \
-	  batterywarning \
-	  SchedulePowerOnOff \
-	  BatteryWarning \
-	  SDKGallery \
-	  Gallery2 \
-	  MtkBt \
-	  mtkbt \
-	  SoundRecorder \
-	  Calendar
+          Calendar \
+          Music \
+          MusicFX \
+          batterywarning \
+          SchedulePowerOnOff \
+          BatteryWarning \
+          SDKGallery \
+          Gallery2 \
+          MtkBt \
+          mtkbt \
+          SoundRecorder \
+          Calendar
+#add by Heaven start
+    Y_REMOVE_PRODUCT_PACKAGES += \
+          WallpaperCropper \
+          DownloadProvider \
+          CalendarProvider \
+          BasicDreams \
+          DownloadProviderUi \
+          CalendarImporter \
+          PrintSpooler \
+          BSPTelephonyDevTool \
+          AtciService \
+          LiveWallpapersPicker \
+          LiveWallpapers \
+          VisualizationWallpapers \
+          Galaxy4 \
+          HoloSpiralWallpaper \
+          NoiseField \
+          PhaseBeam \
+          MTKThermalManager \
+          SmartcardService \
+          DrmProvider \
+          Omacp \
+          OneTimeInitializer \
+          KeyChain \
+          ManagedProvisioning \
+          EngineerMode \
+          Contacts \
+          PhotoTable \
+          FMRadio \
+          HTMLViewer \
+          LatinIME \
+          Stk1 \
+          VpnDialogs \
+          DeskClock \
+          Email \
+          QuickSearchBox \
+          FwkPlugin \
+          FWUpgrade \
+          PacProcessor \
+          NlpService \
+          ProxyHandler \
+          FWUpgradeProvider \
+          UserDictionaryProvider \
+          MTKAndroidSuiteDaemon \
+          Bluetooth
+#add by Heaven end
   endif
 
   ifeq ($(strip $(call get_yov_board)), ms16)
     Y_REMOVE_PRODUCT_PACKAGES += \
-      Calendar \
-      Launcher3\
-      Home\
-	  Music \
-	  MusicFX \
-	  Gallery2 \
-	  SoundRecorder \
-	  Calendar \
-	  Email \
-	  kedaxunfei\
-	  BtReverse\
-	  driveRecorder\
-	  EmailDrmAddon\
-	  NoiseField\
-	  LiveWallpapers\
-	  LiveWallpapersPicker\
-	  PhaseBeam\
-	  PicoTts\
-	  PrintSpooler
+          Calendar \
+          Launcher3\
+          Home\
+          Music \
+          MusicFX \
+          Gallery2 \
+          SoundRecorder \
+          Calendar \
+          Email \
+          kedaxunfei\
+          BtReverse\
+          driveRecorder\
+          EmailDrmAddon\
+          NoiseField\
+          LiveWallpapers\
+          LiveWallpapersPicker\
+          PhaseBeam\
+          PicoTts\
+          PrintSpooler
   endif
 
   ifeq ($(strip $(call get_yov_board)), k68c)
     Y_REMOVE_PRODUCT_PACKAGES += \
-	  Home \
-	  Launcher2 \
-	  Launcher3 \
-	  DeskClock \
-	  Email \
-	  Exchange2 \
-	  QuickSearchBox \
-	  HoloSpiralWallpaper \
-	  LiveWallpapers \
-	  LiveWallpapersPicker \
-	  MagicSmokeWallpapers \
-	  Music \
-	  MusicFX
+          Home \
+          Launcher2 \
+          Launcher3 \
+          DeskClock \
+          Email \
+          Exchange2 \
+          QuickSearchBox \
+          HoloSpiralWallpaper \
+          LiveWallpapers \
+          LiveWallpapersPicker \
+          MagicSmokeWallpapers \
+          Music \
+          MusicFX
   endif
 endif
