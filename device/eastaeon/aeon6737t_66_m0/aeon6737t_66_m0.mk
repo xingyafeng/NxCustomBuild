4bfcc5b45ef41d7f5f832c65aaa9dd1cac3489d2
xingyafeng <xingyf@yunovo.cn>
2019-03-21 17:52:36 +0800

    统一平台差异文件[$(TARGET_DEVICE).mk]
    Change-Id: If9a7288e51f725b0b2c36783161499e3bddb4938

--- device/eastaeon/aeon6737t_66_m0/aeon6737t_66_m0.mk
+++ device/eastaeon/aeon6737t_66_m0/aeon6737t_66_m0.mk
@@ -1,5 +1,7 @@
-## 自定义客制化 config.mk
-$(info including $(call my-dir)/config.mk ...)
+# 平台差异模块
+# 当增加模块不具支持所有平台时,需要单独以平台为单位增加.
+# 若增加模块具有通用性, 此时就需要增加到common/commom.mk 中. <注意>
+$(info including $(call my-dir)/$(TARGET_DEVICE).mk ...)
 
 ## yunovo-common
 YUNOVO_COMMON_LIB  := $(notdir $(call find-file-folder, yunovo, 2, yunovo-common))
