b409520948a374308aa85e67e5fa54cdaf4aed55
xingyafeng <xingyf@yunovo.cn>
2019-03-29 18:42:58 +0800

    增加封板标签属性[ro.release.tag]
    Change-Id: I8a915cbe2c066afac0b884070c6947a3454b6fd8

--- tools/yunovoinfo.sh
+++ tools/yunovoinfo.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # YUNOVO_S:add by yafeng 2017.6.10
-if [ -n "$YUNOVO_PRJ_NAME" ];then
+if [[ -n "$YUNOVO_PRJ_NAME" ]];then
  echo "ro.yunovo.prj.name=$YUNOVO_PRJ_NAME"
 fi
 
-if [ -n "$YUNOVO_SYSTEM_VERSION_FOTA" ];then
+if [[ -n "$YUNOVO_SYSTEM_VERSION_FOTA" ]];then
  echo "ro.nxos.version=$YUNOVO_SYSTEM_VERSION_FOTA"
 fi
+
+if [[ -n "$YUNOVO_RELEASE_TAG" ]];then
+ echo "ro.release.tag=$YUNOVO_RELEASE_TAG"
+fi
+
 # YUNOVO_E:add by yafeng 2017.6.10
