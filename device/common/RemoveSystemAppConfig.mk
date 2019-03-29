429b753a3bc4508dba2cec43bbd2ba137137127d
shihanwen <shihanwen@yunovo.cn>
2019-03-29 16:02:10 +0800

    [Heaven]Update:MS16平台删除一些多余的应用
    Change-Id: I2765aa05552d34282b4521ac1794330493d4b4ed

--- device/common/RemoveSystemAppConfig.mk
+++ device/common/RemoveSystemAppConfig.mk
@@ -5,7 +5,6 @@ ifneq ($(strip $(SPT_VERSION_NO)),)
 
   ifneq ($(filter $(call get_yov_board), cm01 cm02), )
     Y_REMOVE_PRODUCT_PACKAGES += \
-          Calendar \
           Music \
           MusicFX \
           batterywarning \
@@ -86,7 +85,41 @@ ifneq ($(strip $(SPT_VERSION_NO)),)
           BrowserXposed \
           DeskClock \
           AndroidAudioRecorder \
-          Galaxy4
+          Galaxy4 \
+          PerfMon_v1.21 \
+          MMITest \
+          LatinIME \
+          HoloSpiralWallpaper \
+          webview \
+          NoteBook \
+          KeyChain \
+          Omacp \
+          UserDictionaryProvider \
+          ProxyHandler \
+          PacProcessor \
+          SprdQuickSearchBox \
+          VpnDialogs \
+          HTMLViewer \
+          FMRadio \
+          PhotoTable \
+          ManagedProvisioning \
+          OneTimeInitializer \
+          BasicDreams \
+          CalendarProvider \
+          WallpaperCropper \
+          ContactsBlackListAddon \
+          DialerBlackListAddon \
+          Calculator \
+          SmilPlayer \
+          modemassert \
+          CallLogBackup \
+          CellBroadcastReceiver \
+          messaging \
+          WAPPushManager \
+          CarrierConfig \
+          Stk \
+          StatementService \
+          Camera2
   endif
 
   ifeq ($(strip $(call get_yov_board)), k68c)
