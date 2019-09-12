3f8bfba26fdd9d97ac79a66ca6e0ce71dc3dda0d
wujie <wujie@yunovo.cn>
2019-09-12 11:58:08 +0800

    修改属性配置
    Change-Id: I708b9343fb5f32fca39abc432dc5b214507629b3

--- device/common/NxCustomResource.mk
+++ device/common/NxCustomResource.mk
@@ -59,7 +59,7 @@ PRODUCT_BOOT_JARS += \
 
 # yunovo蓝牙协议栈
 ifeq (1,$(filter 1,$(shell echo "$$(( $(PLATFORM_SDK_VERSION) > 23 ))" )))
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.start.yun_vendor_bt=1
+ADDITIONAL_BUILD_PROPERTIES += persist.sys.start.yun_vendor_bt=1
 PRODUCT_PACKAGES += \
 	yunbluetooth.default
 
