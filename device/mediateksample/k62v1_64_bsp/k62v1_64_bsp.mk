2fd1b5fac0550eb4b3790aa49aac4916f0f7c508
baiwei <baiw@yunovo.cn>
2019-11-08 16:56:54 +0800

    [NxCustomBuild] 添加客制化定义 | add custom config
    Change-Id: Ibd72213f327f86476b950739c94264fdbb5092cf

--- device/mediateksample/k62v1_64_bsp/k62v1_64_bsp.mk
+++ device/mediateksample/k62v1_64_bsp/k62v1_64_bsp.mk
@@ -7,3 +7,39 @@ $(info including $(call my-dir)/$(TARGET_DEVICE).mk ...)
 # 增加水印浮窗
 PRODUCT_PACKAGES += yunovo-services
 PRODUCT_SYSTEM_SERVER_JARS += yunovo-services
+
+## wilber start, #{
+$(warning "YUNOVO_CUSTOM = ${YUNOVO_CUSTOM}")
+ifeq ($(YUNOVO_CUSTOM), yes)
+PRODUCT_PACKAGES += libsys_yov
+
+#PRODUCT_DEFAULT_PROPERTY_OVERRIDES
+#PRODUCT_PROPERTY_OVERRIDES += ro.yov.sys.custom=true
+ADDITIONAL_BUILD_PROPERTIES += ro.yov.sys.custom=true
+
+# 语言中文
+PRODUCT_LOCALES := zh_CN en_US zh_TW
+
+# 性能优化：任务快照
+ADDITIONAL_BUILD_PROPERTIES += persist.enable_task_snapshots=false
+
+# 取消导航栏
+ifeq ($(TARGET_BUILD_VARIANT),user)
+ADDITIONAL_BUILD_PROPERTIES += qemu.hw.mainkeys=1
+else
+ADDITIONAL_BUILD_PROPERTIES += qemu.hw.mainkeys=0
+endif
+
+# 默认时区
+ADDITIONAL_BUILD_PROPERTIES += persist.sys.timezone=Asia/Shanghai
+
+# 强制设为横屏
+ADDITIONAL_BUILD_PROPERTIES += yov.sys.force_def_orientation=true
+
+# 增加系統不休眠選項
+ADDITIONAL_BUILD_PROPERTIES += yunovo.support.nerver_screenoff=true
+
+# 禁用搜索引擎管理服务,先关闭,com/android/internal/app/AssistUtils.java:160引用报错
+#ADDITIONAL_BUILD_PROPERTIES += config.disable_searchmanager=true
+endif
+### wilber end #}
