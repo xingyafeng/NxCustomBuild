8569a99081c8fe6540d70139014d22388e5fd878
xingyafeng <xingyf@yunovo.cn>
2019-11-15 14:39:21 +0800

    [yafeng]:修复当裁剪的版本不存在的时候，不应该使编译退出而是继续执行.
    Change-Id: I72d2247b85e8909edbf80048039826ab58992bbc

--- device/common/RemoveSystemAppConfig.mk
+++ device/common/RemoveSystemAppConfig.mk
@@ -3,6 +3,6 @@
 # ----  系统裁剪模块
 ifeq ($(strip $(IS_PUBLIC_VERSION)), false)
 
-include $(YUNOVO_ROOT)/$(YUNOVO_BUILD)/$(YUNOVO_COMMON)/config/$(strip $(call get_yov_board)).mk
+-include $(YUNOVO_ROOT)/$(YUNOVO_BUILD)/$(YUNOVO_COMMON)/config/$(strip $(call get_yov_board)).mk
 
 endif
