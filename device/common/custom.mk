1a4baeed2ed17fce7cf933a97410841062ced5c9
xingyafeng <xingyf@yunovo.cn>
2019-11-22 12:46:54 +0800

    增加ODM分区客制化的定制
    Change-Id: I4b5005652b3837c2b06afe79a48ff9a6ed4d669b

--- device/common/custom.mk
+++ device/common/custom.mk
@@ -10,12 +10,15 @@ PRODUCT_COPY_FILES  += $(foreach fs, $(YUNOVO_SYSTEM_FILES), $(YUNOVO_COMMON_P)/
 YUNOVO_CUSTOM_FILES := $(shell if [ -d $(YUNOVO_COMMON_P)/custom ]; then cd $(YUNOVO_COMMON_P)/custom && find * -type f; fi)
 PRODUCT_COPY_FILES  += $(foreach fs, $(YUNOVO_CUSTOM_FILES), $(YUNOVO_COMMON_P)/custom/$(fs):custom/$(fs))
 
-## 2. Zen平台下 system custom
+## 2. Zen平台下 system custom odm oem
 YUNOVO_ZEN_P = $(YUNOVO_ROOT)/$(YUNOVO_CONFIG)/$(YUNOVO_CUSTOM_P)
 YUNOVO_ZEN_OVERRIDE_P = $(YUNOVO_ZEN_P)/override
 
 YUNOVO_SYSTEM_FILES := $(shell if [ -d $(YUNOVO_ZEN_P)/oem ]; then cd $(YUNOVO_ZEN_P)/oem && find * -type f; fi)
 PRODUCT_COPY_FILES  += $(foreach fs, $(YUNOVO_SYSTEM_FILES), $(YUNOVO_ZEN_P)/oem/$(fs):oem/$(fs))
+
+YUNOVO_SYSTEM_FILES := $(shell if [ -d $(YUNOVO_ZEN_P)/odm ]; then cd $(YUNOVO_ZEN_P)/odm && find * -type f; fi)
+PRODUCT_COPY_FILES  += $(foreach fs, $(YUNOVO_SYSTEM_FILES), $(YUNOVO_ZEN_P)/odm/$(fs):odm/$(fs))
 
 YUNOVO_SYSTEM_FILES := $(shell if [ -d $(YUNOVO_ZEN_P)/custom ]; then cd $(YUNOVO_ZEN_P)/custom && find * -type f; fi)
 PRODUCT_COPY_FILES  += $(foreach fs, $(YUNOVO_SYSTEM_FILES), $(YUNOVO_ZEN_P)/custom/$(fs):custom/$(fs))
