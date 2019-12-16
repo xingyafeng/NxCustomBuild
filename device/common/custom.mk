0ae1e8302b46e7bd44227a18519e029859b3355a
xingyafeng <xingyf@yunovo.cn>
2019-12-16 15:37:30 +0800

    [yafeng]: 修复OEM分区客制化拷贝失败的问题.
    Change-Id: Icbf21e7005e68c0a2b59a1568870e2dc28bce619

--- device/common/custom.mk
+++ device/common/custom.mk
@@ -4,7 +4,7 @@ $(info including $(call my-dir)/custom.mk ...)
 ## 1. 产品目录下 system custom odm oem 公共部分
 YUNOVO_COMMON_P := $(YUNOVO_ROOT)/$(YUNOVO_BUILD)/$(YUNOVO_COMMON)
 
-YUNOVO_SYSTEM_FILES := $(shell if [ -d $(YUNOVO_COMMON_P)/oem ]; ]; then cd $(YUNOVO_COMMON_P)/oem ]; && find * -type f; fi)
+YUNOVO_SYSTEM_FILES := $(shell if [ -d $(YUNOVO_COMMON_P)/oem ]; then cd $(YUNOVO_COMMON_P)/oem && find * -type f; fi)
 PRODUCT_COPY_FILES  += $(foreach fs, $(YUNOVO_SYSTEM_FILES), $(YUNOVO_COMMON_P)/oem/$(fs):oem/$(fs))
 
 YUNOVO_CUSTOM_FILES := $(shell if [ -d $(YUNOVO_COMMON_P)/odm ]; then cd $(YUNOVO_COMMON_P)/odm && find * -type f; fi)
