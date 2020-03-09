8e4fb53062e4e485bf75fbd55b0773e0e105dfeb
baiwei <baiw@yunovo.cn>
2020-03-09 14:02:19 +0800

    [wilber][exfat] 添加exfat编译支持 | support exfat compile
    Change-Id: I14027146c7462b25ef588d92a9d17c948c078af2

--- device/common/user/xiongkaihao.mk
+++ device/common/user/xiongkaihao.mk
@@ -9,3 +9,12 @@ PRODUCT_PACKAGES += libstagefright_soft_mjpeg
 
 # 背光
 PRODUCT_PACKAGES += libyov lights.$(TARGET_BOARD_PLATFORM)
+
+# 支持exfat
+ifneq ($(strip $(YUNOVO_EXFAT_DISABLE)),true)
+ADDITIONAL_BUILD_PROPERTIES += yov.sys.exfat_enable=true
+
+PRODUCT_PACKAGES += libfuse
+PRODUCT_PACKAGES += mkfs.exfat  fsck.exfat  mount.exfat  libexfat
+PRODUCT_PACKAGES += mkfs.ntfs   fsck.ntfs  mount.ntfs  libntfs-3g fsck.ntfs
+endif
